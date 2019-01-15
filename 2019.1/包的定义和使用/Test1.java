db66fce9bc7903ad6227b84d597a6b21b2f1a3b4
dangwuxie <974079012@qq.com>
2019-01-15 22:08:57 +0800

    包的练习与使用

---
+++ 2019.1/包的定义和使用/Test1.java
@@ -0,0 +1,8 @@
+package www.bit.java;
+import www.bit.java.util.Test2;
+public class Test1 extends Test2{
+    public static void main(String[] args){
+        Test1 test1 = new Test1();
+        System.out.println(test1.school);
+    }
+}
---
+++ 2019.1/包的定义和使用/Test2.java
@@ -0,0 +1,11 @@
+package www.bit.java.util;
+public class Test2{
+    private int age = 20;
+    public String name = "test";
+    protected String school = "qinghua";
+    public void fun(){
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(school);
+    }
+}
