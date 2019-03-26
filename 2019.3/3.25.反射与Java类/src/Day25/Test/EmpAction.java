046c2c2e1523397118d3eb472062daff0ed7f7e6
dangwuxie <974079012@qq.com>
2019-03-27 00:36:49 +0800

    反射与简单Java类

--- 2019.3/3.25.反射与Java类/src/Day25/Test/EmpAction.java
+++ 2019.3/3.25.反射与Java类/src/Day25/Test/EmpAction.java
@@ -1,13 +1,15 @@
 package Day25.Test;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * 操作Emp的类，最终提供给用户使用；
  */
 public class EmpAction {
     private Emp emp = new Emp();
     //设置属性
-    public void setEmpValue(String str){
-
+    public void setEmpValue(String str) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        BeanUtil.setBeanValue(this,str);
     }
     //返回设置后的属性
     public Emp getEmp(){
