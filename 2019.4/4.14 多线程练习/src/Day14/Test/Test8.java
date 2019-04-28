47c4638a7f46effee4581f2f3e4f530ff4429ca3
dangwuxie <974079012@qq.com>
2019-04-28 17:12:34 +0800

    Callable接口实现多线程课件最后练习；

--- 2019.4/4.14 多线程练习/src/Day14/Test/Test8.java
+++ 2019.4/4.14 多线程练习/src/Day14/Test/Test8.java
@@ -1,9 +1,25 @@
 package Day14.Test;
 //Callable 实现多线程;
 
+import java.util.concurrent.*;
 
+class MyThread8 implements Callable{
+    private Integer tickets = 100;
+
+    @Override
+    public Object call() throws Exception {
+        while (tickets > 0){
+            System.out.println(Thread.currentThread().getName()+
+                    "剩余票数："+this.tickets--);
+        }
+        return "卖完了";
+    }
+}
 public class Test8 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        FutureTask<String> task = new FutureTask<>(new MyThread8());
+        new Thread(task,"线程1").start();
+        new Thread(task,"线程2").start();
+        System.out.println(task.get());
     }
 }
